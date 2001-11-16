acdb02b1e8dcad7041ad55618fe764bbdeeacc04
Vincent Favre-Nicolin <vincefn@gmail.com>
2001-11-16 10:10:47 +0000

    Modified Chronometer so that it correctly returns longer elapsed time. But now time shorter than 100s is reported in processor (subjective) time, and time longer than 100s is reported in real time.

--- ObjCryst/Quirks/Chronometer.h
+++ ObjCryst/Quirks/Chronometer.h
@@ -3,35 +3,46 @@
 
 
 #include <stdlib.h>
-#include <ctime>
+#include <time.h>
 #include <iostream.h>
 
-class Chronometer;
 class Chronometer
 {
 	public:
 		Chronometer(){this->start();};
 		~Chronometer(){};
-		void start() {mPaused=false;time0=clock();};
-		void pause() {time1=clock();mPaused=true;};
-		void resume(){time0=clock()-(time1-time0);mPaused=false;};
+		void start() {mPaused=false;mTime0=clock();mTimeSec0=time(0);}
+		void pause() {mTime1=clock();mTimeSec1=time(0);mPaused=true;}
+		void resume()
+		{
+			mTime0=clock()-(mTime1-mTime0);
+			mTimeSec0=time(0)-(mTimeSec1-mTimeSec0);
+			mPaused=false;
+		}
 		void print() 
 		{
-         if(mPaused == false) time1=clock();
+         if(mPaused == false) mTime1=clock();
 			cout.setf(ios::fixed);
 			int tmp=cout.precision(2);
-			cout << "Elapsed time : " << (time1-time0)/(double)CLOCKS_PER_SEC << " s."<<endl ;
+			cout << "Elapsed time : " << this->seconds() << " s."<<endl ;
 			cout.precision(tmp);
 			cout.unsetf(ios::fixed);
-		};
+		}
 		double seconds()
       {
-         if(mPaused ==false) time1=clock();
-         return (time1-time0)/(double)CLOCKS_PER_SEC;
-      };
+         if(mPaused ==false)
+			{
+				mTime1=clock();
+				mTimeSec1=time(0);
+			}
+			if((mTimeSec1-mTimeSec0)>100) return (mTimeSec1-mTimeSec0);
+         return (mTime1-mTime0)/(double)CLOCKS_PER_SEC;
+      }
 	private:
-		clock_t time0;
-		clock_t time1;
+		clock_t mTime0;
+		clock_t mTime1;
+		time_t mTimeSec0;
+		time_t mTimeSec1;
       bool mPaused;
 };
 
