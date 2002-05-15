a53f6addfc1371eea21b2e00848c60e69c12c09f
Vincent Favre-Nicolin <vincefn@gmail.com>
2002-05-15 21:35:25 +0000

    Changed Fox version number.

--- ObjCryst/wxCryst/Fox.cpp
+++ ObjCryst/wxCryst/Fox.cpp
@@ -167,7 +167,7 @@ bool MyApp::OnInit()
 
    WXCrystMainFrame *frame ;
    
-   frame = new WXCrystMainFrame("FOX: Free Objects for Xtal structures v1.1.2",
+   frame = new WXCrystMainFrame("FOX: Free Objects for Xtal structures v1.2",
                                  wxPoint(50, 50), wxSize(550, 400));
    // Use the main frame status bar to pass messages to the user
       pMainFrameForUserMessage=frame;
@@ -389,7 +389,7 @@ void WXCrystMainFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 {
    wxString msg;
    msg.Printf( _T("F.O.X. - Free Objects for Xtal structures\n")
-               _T("Version 1.1.2\n\n")
+               _T("Version 1.2\n\n")
                _T("(c) 2000-2002 Vincent FAVRE-NICOLIN, vincefn@users.sourceforge.net\n")
                _T("    2000-2001 Radovan CERNY, University of Geneva\n\n")
                _T("http://objcryst.sourceforge.net\n")
