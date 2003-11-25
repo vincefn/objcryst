e42ce9f6925f3f372a69292b51e545713b61d987
Vincent Favre-Nicolin <vincefn@gmail.com>
2003-11-25 21:00:29 +0000

    Added test file with functions to test the speed of ObjCryst++/Fox.

---
+++ ObjCryst/ObjCryst/test.cpp
@@ -0,0 +1,168 @@
+/*  ObjCryst++ Object-Oriented Crystallographic Library
+    (c) 2000-2002 Vincent Favre-Nicolin vincefn@users.sourceforge.net
+        2000-2001 University of Geneva (Switzerland)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation;  version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*   test.cpp
+*  source file for test functions (speed, etc...)
+*
+*/
+#include <stdlib.h>
+#include <list>
+#include "ObjCryst/test.h"
+#include "ObjCryst/Crystal.h"
+#include "ObjCryst/Atom.h"
+#include "ObjCryst/DiffractionDataSingleCrystal.h"
+#include "ObjCryst/PowderPattern.h"
+#include "RefinableObj/GlobalOptimObj.h"
+#include "Quirks/VFNStreamFormat.h"
+
+namespace ObjCryst
+{
+
+SpeedTestReport SpeedTest(const unsigned int nbAtom, const int nbAtomType,const string spacegroup,
+                          const RadiationType radiation, const unsigned long nbReflections,
+                          const unsigned int dataType,const REAL time)
+{
+   Crystal cryst(9,11,15,1.2,1.3,1.7,spacegroup);
+   for(int i=0;i<nbAtomType;++i)
+   {
+      cryst.AddScatteringPower(new ScatteringPowerAtom("O","O",1.5));
+   }
+   for(int i=0;i<nbAtomType;++i)
+   {
+      cryst.AddScatterer(new Atom(.0,.0,.0,"O",
+                                  &(cryst.GetScatteringPowerRegistry().GetObj(i%nbAtomType)),
+                                  1.));
+   }
+   cryst.SetUseDynPopCorr(false);
+   
+   RefinableObj *pData;
+   switch(dataType)
+   {
+      case 0:
+      {
+         DiffractionDataSingleCrystal *pDataTmp=new DiffractionDataSingleCrystal;
+         pDataTmp->SetWavelength(0.25);
+         pDataTmp->SetRadiationType(radiation);
+         pDataTmp->SetMaxSinThetaOvLambda(100.);
+         pDataTmp->SetCrystal(cryst);
+         float maxtheta=0.1;
+         for(;;)
+         {
+            pDataTmp->GenHKLFullSpace(maxtheta, true);
+            if(pDataTmp->GetNbRefl()>(long)nbReflections) break;
+            maxtheta*=1.5;
+            if(maxtheta>=M_PI/2.) break;
+         }
+         CrystVector_REAL hh; hh=pDataTmp->GetH();hh.resizeAndPreserve(nbReflections);hh+=0.0001;
+         CrystVector_REAL kk; kk=pDataTmp->GetK();kk.resizeAndPreserve(nbReflections);kk+=0.0001;
+         CrystVector_REAL ll; ll=pDataTmp->GetL();ll.resizeAndPreserve(nbReflections);ll+=0.0001;
+
+         CrystVector_long h(nbReflections); h=hh;
+         CrystVector_long k(nbReflections); k=kk;
+         CrystVector_long l(nbReflections); l=ll;
+
+         CrystVector_REAL iobs(nbReflections);
+         for(unsigned int i=0;i<nbReflections;++i) iobs(i)=(REAL)rand();
+         CrystVector_REAL sigma(nbReflections);sigma=1;
+
+         pDataTmp->SetHklIobs (h, k, l, iobs, sigma);
+         pDataTmp->SetWeightToInvSigma2();
+         
+         pData=pDataTmp;
+         break;
+      }
+      case 1:
+      {
+         PowderPattern *pDataTmp=new PowderPattern;
+         pDataTmp->SetWavelength(0.25);
+         pDataTmp->SetRadiationType(radiation);
+         pDataTmp->SetPowderPatternPar(0.001,.001,3140);
+         CrystVector_REAL iobs(3140);
+         for(unsigned int i=0;i<3140;++i) iobs(i)=(REAL)rand()+1.;
+         pDataTmp->SetPowderPatternObs(iobs);
+         pDataTmp->SetMaxSinThetaOvLambda(100.);
+
+         PowderPatternBackground *backgdData= new PowderPatternBackground;
+         backgdData->SetName("PbSo4-background");
+         pDataTmp->AddPowderPatternComponent(*backgdData);
+         
+         PowderPatternDiffraction * diffData=new PowderPatternDiffraction;
+         diffData->SetCrystal(cryst);
+         pDataTmp->AddPowderPatternComponent(*diffData);
+         diffData->SetName("Crystal phase");
+         diffData->SetReflectionProfilePar(PROFILE_PSEUDO_VOIGT,
+                                           .03*DEG2RAD*DEG2RAD,0.,0.,0.3,0);
+         {
+            float maxtheta=0.1;
+            for(;;)
+            {
+               diffData->ScatteringData::GenHKLFullSpace(maxtheta, true);
+               if(diffData->GetNbRefl()>(long)nbReflections) break;
+               maxtheta*=1.5;
+               if(maxtheta>=M_PI/2.) break;
+            }
+            CrystVector_REAL hh; hh=diffData->GetH();hh.resizeAndPreserve(nbReflections);
+            CrystVector_REAL kk; kk=diffData->GetK();kk.resizeAndPreserve(nbReflections);
+            CrystVector_REAL ll; ll=diffData->GetL();ll.resizeAndPreserve(nbReflections);
+
+            diffData->SetHKL (hh, kk, ll);
+         }
+         pData=pDataTmp;
+         break;
+      }
+   }
+   
+   //Create the global optimization object
+      MonteCarloObj globalOptObj;
+      globalOptObj.AddRefinableObj(*pData);
+      globalOptObj.AddRefinableObj(cryst);
+
+   //Refine only positionnal parameters
+      globalOptObj.FixAllPar();
+      globalOptObj.SetParIsFixed(gpRefParTypeScattTransl,false);
+      globalOptObj.SetParIsFixed(gpRefParTypeScattOrient,false);
+
+   //Don't cheat ;-)
+      globalOptObj.RandomizeStartingConfig();
+   
+   //Annealing parameters (schedule, Tmax, Tmin, displacement schedule, 
+      globalOptObj.SetAlgorithmParallTempering(ANNEALING_SMART,1e8,1e-8,
+                                               ANNEALING_EXPONENTIAL,8,.125);      
+      
+   //Global Optimization
+      //The real job-first test
+      long nbTrial=50000000;
+      globalOptObj.Optimize(nbTrial,true,0,time);
+      
+   delete pData;
+   
+   SpeedTestReport report;
+   report.mNbAtom=nbAtom;
+   report.mNbAtomType=nbAtomType;
+   report.mSpacegroup=spacegroup;
+   report.mRadiation=radiation;
+   report.mNbReflections=nbReflections;
+   report.mDataType=dataType;
+   report.mBogoMRAPS=(REAL)nbAtom*cryst.GetSpaceGroup().GetNbSymmetrics()*(REAL)nbReflections
+                     *(50000000-nbTrial)/globalOptObj.GetLastOptimElapsedTime()/1e6;
+   report.mBogoMRAPS_reduced=(REAL)nbAtom*cryst.GetSpaceGroup().GetNbSymmetrics(true,true)
+                             *(REAL)nbReflections
+                             *(50000000-nbTrial)/globalOptObj.GetLastOptimElapsedTime()/1e6;
+   report.mBogoSPS=(50000000-nbTrial)/globalOptObj.GetLastOptimElapsedTime();
+   return report;
+}
+}
---
+++ ObjCryst/ObjCryst/test.h
@@ -0,0 +1,71 @@
+/*  ObjCryst++ Object-Oriented Crystallographic Library
+    (c) 2000-2002 Vincent Favre-Nicolin vincefn@users.sourceforge.net
+        2000-2001 University of Geneva (Switzerland)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation;  version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+/*   test.h
+*  header file for test functions (speed, etc...)
+*
+*/
+#ifndef _OBJCRYST_TEST_H_
+#define _OBJCRYST_TEST_H_
+
+#include "ObjCryst/General.h"
+
+namespace ObjCryst
+{
+/** Structure to hold the results of a speedtest (see ObjCryst::SpeedTest())
+*
+*
+*/
+struct SpeedTestReport
+{
+   /// Total number of unique atoms in the test structure.
+   unsigned int mNbAtom;
+   /// Total number of atom types in the test structure.
+   int mNbAtomType;
+   /// The symbol for the spacegroup
+   string mSpacegroup;
+   /// The type of radiation used
+   RadiationType mRadiation;
+   /// The total number of reflections used for the tests
+   unsigned long mNbReflections;
+   /// dataType: 0= single crystal, 1= powder pattern (1 background + 1 crystal phase)
+   unsigned int mDataType;
+   /// Million of Reflections-Atoms computed Per Second (considering all atoms in the unit cell)
+   REAL mBogoMRAPS;
+   /// Million of Reflections-Atoms computed Per Second (considering all atoms in the unit cell,
+   /// except those deduced by a center of symmetry or a lattice translation)
+   REAL mBogoMRAPS_reduced;
+   /// Number of Structures evaluated Per Second
+   REAL mBogoSPS;
+};
+
+/**
+*
+* \param nbAtom: total number of unique atoms.
+* \param nbAtomType: total number of atom types. Must be smaller than nbAtom.
+* \param spacegroup: the symbol or spacegroup number for the spacegroup to be tested.
+* \param radiation: the type of radiation to do the test with (neutron, x-ray).
+* \param nbReflections: total number of reflections to use.
+* \param time: duration of the test (10s is usually enough).
+* \param dataType: 0= single crystal, 1= powder pattern (1 background + 1 crystal phase)
+*/
+SpeedTestReport SpeedTest(const unsigned int nbAtom, const int nbAtomType,const string spacegroup,
+                          const RadiationType radiation, const unsigned long nbReflections,
+                          const unsigned int dataType,const REAL time);
+
+}
+#endif
