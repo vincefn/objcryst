3a9cc860821bb13c3d3588e4bf1a27d20e67320f
Vincent Favre-Nicolin <vincefn@gmail.com>
2008-07-11 18:58:58 +0000

    Revision 1034

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -73,7 +73,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 //:TODO: supply __FOXREVISION__ from the command line (at least under Linux)
-#define __FOXREVISION__ 1029
+#define __FOXREVISION__ 1034
 
 static std::string foxVersion;
 
