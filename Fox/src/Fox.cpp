38f42e4501f2e78d76f01dccfdc90a76f8b3a9db
Vincent Favre-Nicolin <vincefn@gmail.com>
2009-03-28 15:27:59 +0000

    Release 1.8.1

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -80,7 +80,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 //:TODO: supply __FOXREVISION__ from the command line (at least under Linux)
-#define __FOXREVISION__ 1108
+#define __FOXREVISION__ 1113
 
 static std::string foxVersion;
 
@@ -300,7 +300,7 @@ int main (int argc, char *argv[])
    
    {// Fox version
       char verBuf[200];
-      sprintf(verBuf,"1.8.X-#%d",__FOXREVISION__);
+      sprintf(verBuf,"1.8.1-#%d",__FOXREVISION__);
       foxVersion=verBuf;
    }
    bool useGUI(true);
