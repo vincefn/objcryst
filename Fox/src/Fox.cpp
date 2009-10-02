91e51f8512f39a3d3144a14ef351c1177c38bda8
Vincent Favre-Nicolin <vincefn@gmail.com>
2009-10-02 19:31:30 +0000

    Fox, revision 1180

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -80,7 +80,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 //:TODO: supply __FOXREVISION__ from the command line (at least under Linux)
-#define __FOXREVISION__ 1175
+#define __FOXREVISION__ 1180
 
 static std::string foxVersion;
 
