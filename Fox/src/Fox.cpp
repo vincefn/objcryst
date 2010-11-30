df6cc7c7d536f9780b5b3d47273e9d55f868b48e
Vincent Favre-Nicolin <vincefn@gmail.com>
2010-11-30 21:15:09 +0000

    Update beta version

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -83,7 +83,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 //:TODO: supply __FOXREVISION__ from the command line (at least under Linux)
-#define __FOXREVISION__ 1269
+#define __FOXREVISION__ 1273
 
 static std::string foxVersion;
 
