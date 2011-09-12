ae5ddd2f0a36244409e69a60f89607990dfa516f
Vincent Favre-Nicolin <vincefn@gmail.com>
2011-09-12 19:06:27 +0000

    Fox release 1.9.7

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -84,7 +84,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 //:TODO: supply __FOXREVISION__ from the command line (at least under Linux)
-#define __FOXREVISION__ 1305
+#define __FOXREVISION__ 1313
 
 static std::string foxVersion;
 
