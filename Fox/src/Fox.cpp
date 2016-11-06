fb82bef029fc3d5a7ae8caa79011903f27baa2ed
Vincent Favre-Nicolin <vincefn@gmail.com>
2016-11-06 15:29:23 +0100

    Try again wxSafeYield...

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -337,7 +337,7 @@ void WXCrystInformUserStdOut(const string &str)
    if(wxThread::IsMain())
    {
       pLogWindowForUserMessage->AppendText(wxString::Format(_T("%s\n"), str.c_str()));
-      wxYield();
+      wxSafeYield(pLogWindowForUserMessage->MacGetTopLevelWindow(), true);
    }
    else
    {
