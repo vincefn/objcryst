db4e82e85e49e9a2902170012bd8d9c19833df58
Vincent Favre-Nicolin <vincefn@gmail.com>
2016-11-13 18:05:21 +0100

    Fox version 2016.2

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -118,7 +118,7 @@ using namespace std;
 // Rough version number - must be updated at least for every major version or critical update
 // This is used to check for updates...
 // Now using YYYY### (4-digit year + 3 number for the version)
-#define __FOXREVISION__ 2016001
+#define __FOXREVISION__ 2016002
 
 static std::string foxVersion;
 
