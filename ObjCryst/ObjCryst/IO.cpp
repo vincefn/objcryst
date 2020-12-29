65402aca200633341d8766e6cab57b4424e76742
Vincent FAVRE-NICOLIN <favre@esrf.fr>
2020-12-29 15:51:42 +0100

    Prepare Fox 2020.1

--- ObjCryst/ObjCryst/IO.cpp
+++ ObjCryst/ObjCryst/IO.cpp
@@ -123,7 +123,7 @@ void XMLCrystFileSaveGlobal(ostream &out)
    char strDate[40];
    strftime(strDate,sizeof(strDate),"%Y-%m-%dT%H:%M:%S%Z",gmtime(&date));//%Y-%m-%dT%H:%M:%S%Z
    tag.AddAttribute("Date",strDate);
-   tag.AddAttribute("Revision","2017002");
+   tag.AddAttribute("Revision","2021001");
    out<<tag<<endl;
 
    for(int i=0;i<gCrystalRegistry.GetNb();i++)
