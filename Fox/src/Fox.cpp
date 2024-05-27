4e031e93a378f44a4fb6a6de5dfb6519ecefb945
janrohlicek <honzicek.rohlicek@seznam.cz>
2024-05-27 15:12:33 +0200

    Rwp and port option added to cmd line

--- Fox/src/Fox.cpp
+++ Fox/src/Fox.cpp
@@ -593,6 +593,7 @@ int main (int argc, char *argv[])
    string outfilename("Fox-out.xml");
    string working_dir("");
    long filenameInsertCost=-1;
+   long filenameInsertRwp=-1;
    bool randomize(false);
    bool only3D(false);
    bool loadFourierGRD(false);
@@ -610,6 +611,7 @@ int main (int argc, char *argv[])
    bool runclient(false);
    long nbCPUs = -1;
    string IP;
+   string port;
    bool testLSQ=false;
    bool testMC=false;
    bool testSPEED=false;
@@ -622,12 +624,23 @@ int main (int argc, char *argv[])
 
          if(!useGUI) {
             cout << "Client output: Run client with GUI only!"<<endl;
-            cout << "i.e. Fox --runclient 10.0.0.1 --CPUs 4 --working_dir c:\\FOXGrid"<<endl;
+            cout << "i.e. Fox --runclient 10.0.0.1:2854 --CPUs 4 --working_dir c:\\FOXGrid"<<endl;
             exit(0);
          }
          runclient = true;
          i++;
-         IP = string(wxString(argv[i]).ToAscii());
+  string tmpIP = string(wxString(argv[i]).ToAscii());
+
+         size_t colonPos = tmpIP.find(':');
+         if (colonPos != string::npos) {
+            IP = tmpIP.substr(0, colonPos);
+            port = tmpIP.substr(colonPos + 1);
+         } else {
+            cout << "ERROR: Can't read ip address and port: " << tmpIP << endl;
+            cout << "Use ip with port, e.g. 10.0.0.1:2853 "<< endl;
+            exit(0);
+         }
+
          //get nb of CPUs to use
          if(STRCMP(wxString("--CPUs"),argv[i+1])==0) {
              i=i+2;
@@ -762,6 +775,11 @@ int main (int argc, char *argv[])
          filenameInsertCost = outfilename.find("#cost",0);
          cout <<"Fox:#cost, pos="<<filenameInsertCost<<","<<string::npos<<endl;
          if((long)(string::npos)==filenameInsertCost) filenameInsertCost=-1;
+
+         filenameInsertRwp = outfilename.find("#Rwp",0);
+         cout <<"Fox:#Rwp, pos="<<filenameInsertRwp<<","<<string::npos<<endl;
+         if((long)(string::npos)==filenameInsertRwp) filenameInsertRwp=-1;
+
          continue;
       }
       if(STRCMP("--loadfouriergrd",argv[i])==0)
@@ -1458,6 +1476,20 @@ int main (int argc, char *argv[])
          string tmpstr2=costAsChar;
          tmpstr.replace(filenameInsertCost,5,tmpstr2,0,tmpstr2.length());
       }
+      if(filenameInsertRwp>=0) 
+      {            
+         //update actual position
+         filenameInsertRwp = tmpstr.find("#Rwp",0);
+         char RwpAsChar[50]; 
+         float Rwp = -1;
+         for(unsigned int i=0;i<gPowderPatternRegistry.GetNb();++i) {
+             //TODO: multiphase - now, it take Rwp of the last pattern in the registry...
+            Rwp =  gPowderPatternRegistry.GetObj(i).GetRw();           
+         }
+         sprintf(RwpAsChar,"-Rwp-%.4f", Rwp);
+         string tmpstr2=RwpAsChar;
+         tmpstr.replace(filenameInsertRwp,4,tmpstr2,0,tmpstr2.length());
+      }
       XMLCrystFileSaveGlobal(tmpstr);
       cout <<"End of Fox execution. Bye !"<<endl;
       //TAU_REPORT_STATISTICS();
@@ -1559,6 +1591,7 @@ int main (int argc, char *argv[])
           mpFrame->mpGridWindow->m_WXFoxSlave->setNbCPU(nbCPUs);
       }
       mpFrame->mpGridWindow->m_WXFoxSlave->m_IPWindow->SetValue(wxString::FromAscii(IP.c_str()));
+      mpFrame->mpGridWindow->m_WXFoxSlave->m_portWindow->SetValue(wxString::FromAscii(port.c_str()));
       mpFrame->mpGridWindow->m_WXFoxSlave->OnConnectClient(com);
    }
 
@@ -1656,9 +1689,9 @@ WXCrystMainFrame::WXCrystMainFrame(const wxString& title, const wxPoint& pos, co
       #endif
       //FoxGrid////////////////////////////////////////////////////////////////////
       wxMenu *gridMenu = new wxMenu;
-         gridMenu->Append(MENU_GRID_SERVER_RUN, _T("&Run Server"), _T("Start Fox Grid Server"));
+         gridMenu->Append(MENU_GRID_SERVER_RUN, _T("&Run Master"), _T("Start Fox Grid Master"));
          gridMenu->AppendSeparator();
-         gridMenu->Append(MENU_GRID_CLIENT_START, _T("&Start Client"), _T("Start Fox Grid Client"));
+         gridMenu->Append(MENU_GRID_CLIENT_START, _T("&Start Slave"), _T("Start Fox Grid Slave"));
 
       wxMenu *prefsMenu = new wxMenu;
          prefsMenu->Append(MENU_PREFS_PREFERENCES, _T("&Preferences..."), _T("Fox Preferences..."));
